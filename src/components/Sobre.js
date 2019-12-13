731184206c0ae1a2d3d2a99d26691b2cbd8d1275
moniquealmeida <monique10879@gmail.com>
2019-12-13 10:26:55 -0300

    mudei o texto do sobre

--- src/components/Sobre.js
+++ src/components/Sobre.js
@@ -7,9 +7,16 @@ function Sobre() {
         <div>
             <Nav />
             <div>
-                 <h1 className='hist'>Um pouco sobre a história</h1>
+                 <h1 className='hist'>Um pouco sobre o Dojo</h1>
                  <p>
-                     O dojo Moriyama é ministrado pelo Sensei Shihan Moriyama com foco no karate de Okinawa.
+                 O estilo Sekishin Bujutsu é baseado na Filosofia Samurai, onde a disciplina, esforço, cordialidade e humildade do praticante são avaliados. Não somos um estilo competitivo, pois o nosso objetivo é formar pessoas aptas a contribuir com a sociedade através da prática dos ensinamentos de Shihan Moriyama.
+
+                Os treinos são elaborados para a melhoria do condicionamento físico e mental, além da melhoria da percepção, 
+                para que o aluno possa agir nos momentos decisivos de forma precisa e ágil. Além das práticas de condicionamento 
+                físico, 
+                são oferecidas técnicas de defesa pessoal e manuseio de armas, como Kenjutsu (espada), tantô (punhal) e 
+                bô (bastão)
+
                  </p>
             </div>
             <Footer />
